e4a12ff2f0e4fbe1ed110d31a1b5af00c92b36f9
Steve Pentler <stevepentler@gmail.com>
2016-02-15 00:24:29 -0700

    looking good

--- click_clack.js
+++ click_clack.js
@@ -21,6 +21,7 @@ function draw() {
   var size2 =  50;
   var col = map(level, 0, 1, 0, 255);
 
+
 // vertical line
 stroke(color(0, 0, 255));
   line(size, 0 + level, size , height);
@@ -38,22 +39,47 @@ stroke('magenta');
   triangle(0,height,mouseX,mouseY- size,width,height);
   fill(color(0, 0, 255));
   triangle(0,height,mouseX,mouseY,width,height);
+
+  // triangle
   fill('magenta');
   triangle(0,height,mouseX,mouseY+ 30,width,height);
 
-
-  fill(255);
-  stroke(color(0, 0, 255));
+fill(255);
+stroke(color(0, 0, 255));
   // biggest ellipse
   ellipse(width/2, 3/4*height, 200 - size, 500 - size);
 // smaller ellipse
-  fill('black');
+fill('black');
   ellipse(width/2, 3/4*height, 150, 450);
-  // fill('magenta');
-  fill(0, 0, 255);
+fill(0, 0, 255);
   ellipse(width/2, 3/4*height, 100 - size, 400 - size);
-  fill(0)
+stroke(0)
+fill('magenta')
+  ellipse(width/2, 3/4*height, 50, 50);
+  fill('magenta')
+  ellipse(width/2, 3/4*height, 50, 50);
+
+stroke(color(0, 0, 255));
+fill(0);
   ellipse(width/2, height, 50, 350);
+  // white cirlces
+fill(255);
+  ellipse(width/2, height, 20, 20);
+  ellipse(width/2, height, 10, 10);
+  ellipse(width/2, height - 20, 20, 20);
+  ellipse(width/2, height - 20, 10, 10);
+  ellipse(width/2, height - 40, 20, 20);
+  ellipse(width/2, height - 40, 10, 10);
+  ellipse(width/2, height - 60, 20, 20);
+  ellipse(width/2, height - 60, 10, 10);
+  ellipse(width/2, height - 80, 20, 20);
+  ellipse(width/2, height - 80, 10, 10);
+  ellipse(width/2, height - 100, 20, 20);
+  ellipse(width/2, height - 100, 10, 10);
+  ellipse(width/2, height - 120, 20, 20);
+  ellipse(width/2, height - 120, 10, 10);
+  ellipse(width/2, height - 140, 20, 20);
+  ellipse(width/2, height - 140, 10, 10);
   // triangle(size2,height,size,size,width,height);
   // rotateY(size*.01 + frameCount*.01);
   // torus(200, 60);
@@ -68,12 +94,12 @@ stroke('magenta');
   // endShape();
 }
 
-// function mouseClicked() {
-//   if (value == 0) {
-//     value = 255;
-//     inverseValue = 0;
-//   } else {
-//     value = 0;
-//     inverseValue = 255;
-//   }
-// }
+function mouseClicked() {
+  if (value == 0) {
+    value = 255;
+    inverseValue = 0;
+  } else {
+    value = 0;
+    inverseValue = 255;
+  }
+}
